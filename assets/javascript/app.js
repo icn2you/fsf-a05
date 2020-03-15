4b2c32e2a8eff95b488e348301ac4a8719151504
Christopher Zenner <christopher.zenner@gmail.com>
2020-03-15 15:43:43 -0700

    feat: Add functionality to display Q&A set
    - Create relevant functions to display trivia Q&A set in game engine
      (`app.js`).
    - Tweak UI (`index.html`) to properly display answer options.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 /******************************************************************************
 FSWD:  Christopher B. Zenner
-Date:  03/14/2020
+Date:  03/16/2020
 File:  app.js
 Ver.:  0.1.0 20200311
        
@@ -15,9 +15,9 @@ displays the number of questions answered correctly and incorrectly. The user
 is given the option to play again.
 ******************************************************************************/
 const wonderWomenTriviaInstr = "Click the Start button to begin";
-const dataset = "wonderWomen";
+const dataset = wonderWomen;
 const numQns = 10;
-const timeout = 30;
+const timeout = 15;
 
 class TriviaGame {
   // PROPERTIES
@@ -59,7 +59,14 @@ class TriviaGame {
 
   getTriviaQnA() {
     // Return random trivia Q&A set
-    return this.#dataset[Math.floor(Math.random() * this.#dataset.length)];
+    var index = Math.floor(Math.random() * this.#dataset.length);
+
+    // DEBUG
+    // console.log(this.#dataset);
+    // console.log("size = " + this.#dataset.length);
+    // console.log ("index = " + index);
+
+    return this.#dataset[index];
   }
 
   getCorrectAnsGIF() {
@@ -130,6 +137,7 @@ class TriviaGame {
 
 // Execute script once page is fully loaded
 $(document).ready(function() {
+  // Create game object.
   let wonderWomenTrivia = new TriviaGame("Real Wonder Women Trivia",
     wonderWomenTriviaInstr,
     dataset,
@@ -137,23 +145,50 @@ $(document).ready(function() {
      "https://media.giphy.com/media/5TC1o3oRE68Mg/source.gif", 
      "https://media.giphy.com/media/JzOyy8vKMCwvK/source.gif"]);
 
+  // Initialize stopwatch
   var stopwatch = timeout;
 
+  // ********************************************
+  // displayQnASet() - Trivia question w/answers
+  // ********************************************
+  function displayQnASet(qna) {
+    // DEBUG
+    console.log(qna);
+
+    $("#question").text(qna.question);
+
+    qna.answers.forEach(function(ans, i) {
+      var optID = "opt-0" + (i + 1);
+
+      $("#options").append('<button type="button" class="list-group-item list-group-item-action" id="' + optID + '">');
+      $('#' + optID).text(ans);
+    });
+  }
+
+  // ********************************************
+  // displaySecsLeft() - UI stopwatch
+  // ********************************************
   function displaySecsLeft() {
     stopwatch--;
     $("#stopwatch").text(stopwatch);
   }
 
-  // Once player clicks the Start/Play Again buttons, launch a new game.
+  // Once player clicks the Start Game button, launch a new game.
   $("button").on("click", function(event) {
     $("#game-launchpad").empty();
     $("#stopwatch").text(stopwatch);
 
-    var interval = setInterval(displaySecsLeft, 1000);
-    
+    // Grab interal ID so it can be reset.
+    var intervalID = setInterval(displaySecsLeft, 1000);
+    // Select Q&A set.
+    var qAndAnsSet = wonderWomenTrivia.getTriviaQnA();
+
+    displayQnASet(qAndAnsSet);
+        
+    // Allow user x seconds to answer question such that x = timeout.
     setTimeout(function() {
-      console.log("Time's up!");
-      clearInterval(interval);
-    }, 30000);
+      $("#stopwatch").text("Time's up!");
+      clearInterval(intervalID);
+    }, (timeout * 1000));
   });
 });
