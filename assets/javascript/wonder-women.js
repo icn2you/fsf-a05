9cb0b0aa1d558d2ef8421df5e52b7ab0af10c993
Christopher Zenner <christopher.zenner@gmail.com>
2020-03-14 22:49:55 -0700

    feat: Add JSON dataset

--- assets/javascript/wonder-women.js
+++ assets/javascript/wonder-women.js
@@ -1,222 +1,222 @@
 var wonderWomen = [
   {
-    "woman": "Lorem Ipsum",
-    "photo": "assets/images/lorem.png",
-    "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+    "woman": "Sappho",
+    "photo": "assets/images/sappho.jpg",
+    "question": "Sappho, an ancient Greek woman, was known for:",
     "answers": [
-      "Reprehenderit",
-      "Quaerat iste",
-      "Accusantium odio",
-      "Similique"
+      "Her great lyric poetry, often Lesbian in theme",
+      "Writing The Odyssey and The Iliad",
+      "Being queen of the Greek Island of Lesbos",
+      "Her famous exploits as a warrior and hero of Athens"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Joan of Arc",
+    "photo": "assets/images/joan-of-arc.jpg",
+    "question": "All these things are true about Joan of Arc, except:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "She died of plague in 1341",
+      "She was burned at the stake",
+      "She was part of a relief army at the Siege of Orleans",
+      "She was sainted by the Catholic Church in 1920"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Dolly Parton",
+    "photo": "assets/images/dolly-parton.jpg",
+    "question": "Which of these famous singers had 41 top ten albums, 25 number-one singles, an acting career, and founded an organization promoting girls’ literacy:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Dolly Parton",
+      "Whitney Houston",
+      "Jennifer Lopez",
+      "Cher"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Toni Morrison",
+    "photo": "assets/images/toni-morrison.jpg",
+    "question": "Toni Morrison, acclaimed African-American novelist, won:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Both the Nobel and Pulitzer prizes for her writing",
+      "The Pulitzer Prize for Poetry",
+      "The Nobel Prize in Literature",
+      "Two Pulitzer Prizes in Fiction"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Marie Curie",
+    "photo": "assets/images/marie-curie.jpg",
+    "question": "Marie Curie won two Nobel Prizes for her contributions to which fields:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Chemistry & Physics",
+      "Literature & Peace",
+      "Medicine & Physiology",
+      "She just won one award"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Gloria Steinem",
+    "photo": "assets/images/gloria-steinem.jpg",
+    "question": "Gloria Steinem, the famed American feminist activist, first came to national attention when:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "She went undercover as Playboy Bunny for an exposé",
+      "She burned her brassiere at a public event",
+      "She went on a hunger strike",
+      "Founded Ms. Magazine"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Indira Gandhi",
+    "photo": "assets/images/indira-gandhi.jpg",
+    "question": "Indira Gandhi was:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "The first female Prime Minister of India",
+      "Mahatma Gandhi’s wife and supporter",
+      "Mahatma Gandhi’s daughter and successor",
+      "A revolutionary fighting for Pakistani sovereignty"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Jane Austen",
+    "photo": "assets/images/jane-austen.jpg",
+    "question": "Unrecognized during her lifetime, this English writer wrote many great novels, including Pride and Prejudice:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Jane Austen",
+      "Jane Eyre",
+      "Charlotte Bronte",
+      "Emily Bronte"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Queen Elizabeth II",
+    "photo": "assets/images/queen-elizabeth-ii.jpg",
+    "question": "Who is the longest-reigning British monarch?",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Queen Elizabeth II",
+      "Queen Elizabeth I",
+      "Queen Victoria",
+      "Queen Anne"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Oprah Winfrey",
+    "photo": "assets/images/oprah-winfrey.jpg",
+    "question": "Oprah Winfrey first became a news co-anchor at the young age of:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "19",
+      "16",
+      "21",
+      "23"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "JK Rowling",
+    "photo": "assets/images/jk-rowling.jpg",
+    "question": "The author of the Harry Potter book series chose the pen name “JK Rowling” so as to not reveal her gender. What do the initials “JK” stand for?",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Joanne Kathleen",
+      "Jasmine Kate",
+      "Julianna Keira",
+      "Jennifer Kordelia"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Malala Yousafzai",
+    "photo": "assets/images/malala-yousafzai.jpg",
+    "question": "After surviving an assassination attempt from the Taliban, this Pakistani activist for girls’ education became the youngest recipient of a Nobel Prize ever at the age of 17:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Malala Yousafzai",
+      "Indira Gandhi",
+      "Sharmeen Obaid",
+      "Noor Jehan"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Anne Sullivan",
+    "photo": "assets/images/anne-sullivan.jpg",
+    "question": "Helen Keller was a blind and deaf writer and activist. Who was the young woman who taught her to speak, read Braille, and write?",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Anne Sullivan",
+      "Bertha Thompson",
+      "Catherine Underwood",
+      "Diana Van Houten"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Florence Nightingale",
+    "photo": "assets/images/florence-nightingale.jpg",
+    "question": "Known as the “Lady with the Lamp,” this British, Victorian woman was possibly the most famous and influential nurse in history:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Florence Nightingale",
+      "Mildred Ratched",
+      "Jacqueline Peyton",
+      "Clara Barton"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Ada Lovelace",
+    "photo": "assets/images/ada-lovelace.jpg",
+    "question": "This woman is said to have written the instructions for the first computer program — Black in the mid-1800s:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Ada Love lace",
+      "Hedy Lamar",
+      "Mary Wollstoncraft",
+      "Martha Washington"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Katharine Hepburn",
+    "photo": "assets/images/katharine-hepburn.jpg",
+    "question": "Over a career of more than 60 years, how many Academy Awards did Katharine Hepburn receive?",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "4",
+      "2",
+      "3",
+      "5"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Antonia Novello",
+    "photo": "assets/images/antonia-novello.jpg",
+    "question": "The first woman and first Hispanic American to earn the title of Surgeon General of the United States was:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Antonia Novello",
+      "Beatriz Orozco",
+      "Catalina Perez",
+      "Delfina Quintana"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Whoopi Goldberg",
+    "photo": "assets/images/whoopi-goldberg.jpg",
+    "question": "Which of these entertainers was the first African American woman to receive the coveted EGOT combination of Emmy, Grammy, Oscar, and Tony awards?",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Whoopi Goldberg",
+      "Rita Moreno",
+      "Angela Basset",
+      "Halle Berry"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Miep Gies",
+    "photo": "assets/images/miep-gies.jpg",
+    "question": "This woman helped protect Anne Frank and her family from Nazis during World War II:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Miep Gies",
+      "Karolina Engle",
+      "Liesl Fischer",
+      "Nina Heidegger"
     ]
   },
   {
-    "woman": "",
-    "photo": "",
-    "question": "",
+    "woman": "Katherine Johnson",
+    "photo": "assets/images/katherine-johnson.jpg",
+    "question": "One of Nasa’s brightest “computers,” this African American woman intricately calculated the trajectory of the 1969 Apollo 11 mission to the moon:",
     "answers": [
-      // "ans1",
-      // "ans2",
-      // "ans3",
-      // "ans4"
+      "Katherine Johnson",
+      "Judy Ireland",
+      "Laura Kent",
+      "Marina Langley"
     ]
   }
 ];
