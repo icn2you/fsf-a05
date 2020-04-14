5517dfc902b8e9ee587e9ab2c67ffddb253f518b
Christopher Zenner <christopher.zenner@gmail.com>
2020-04-13 23:25:50 -0700

    chore: increase number of questions served

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ rectly and incorrectly as well as the number of questions left unanswered).
 The user is given the option to play again.
 ******************************************************************************/
 const dataset = wonderWomen;
-const numQns = 3;
+const numQns = 10;
 const timeout = 30; // secs.
 const wonderWomenTriviaInstr = "You will be given a series of " + numQns + " questions regarding famous women in history, accompanied by four (4) possible answers. (Yes, this is a multiple-choice test!) Click the option you think is correct. The game will tell you if your answer is right or wrong. Answer all of the questions correctly, and you may consider yourself a true feminist!";
 
